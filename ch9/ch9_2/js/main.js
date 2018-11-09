67b15faeb6687399ffe6f2a3f951c3fc056c6fc9
sayginguven <sayginguven@yahoo.com>
2018-11-09 12:31:52 -0800

    some events

---
+++ ch9/ch9_2/js/main.js
@@ -0,0 +1,7 @@
+function myfunction(){
+	document.getElementsByClassName("formInputs")[0].style.backgroundColor = "lightblue";
+}
+
+function myAnotherFunction(){
+	document.getElementsByClassName("formInputs")[1].style.backgroundColor = "tomato";
+}
