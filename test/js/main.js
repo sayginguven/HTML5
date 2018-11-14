957e9a78e2d0571c6d56f5580a7e752f2e8d9f81
sayginguven <sayginguven@yahoo.com>
2018-11-14 15:50:19 -0800

    bubble sort

---
+++ test/js/main.js
@@ -0,0 +1,18 @@
+let bubbleSort = function(theArray){
+	do{
+		var swapped = false;
+		let i;
+		for (i=0; i<theArray.length; i+=1){
+			if(theArray[i]> theArray[i+1]){
+				let temp = theArray[i];
+				theArray[i] = theArray[i+1];
+				theArray[i+1] = temp;
+				swapped = true;
+			}
+		}
+	} while(swapped);
+	return theArray;
+};
+
+
+console.log(bubbleSort([5,66,1,-9]));
