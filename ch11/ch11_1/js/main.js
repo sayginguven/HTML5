c43219c3afc781d50ef3b0831afd420c30b88f2c
sayginguven <sayginguven@yahoo.com>
2018-11-14 15:48:36 -0800

    functions

---
+++ ch11/ch11_1/js/main.js
@@ -0,0 +1,50 @@
+function writeMyName(name,lastName){
+
+	console.log(`Hello ${name} ${lastName}`);
+
+}
+
+//es6 arrow function
+let add = function () {
+
+	let arg = arguments.length;
+	let i;
+	let result = 0;
+	for (i = 0 ; i < arg; i+=1) {
+		result+=arguments[i];
+	}
+	return result;
+}	
+
+let mul = function(){
+
+	let arg = arguments.length;
+	let i;
+	let result = 1;
+	for (i = 0 ; i < arg; i+=1) {
+		result*=arguments[i];
+	}
+	return result;
+}
+
+let div = (a=1,b=1) =>{
+	return a/b;
+}
+
+let sub = (a=0,b=0) =>{
+	return a-b;
+}
+
+
+console.log(add(1,2,3,4,5,6,7,8,9,10));
+console.log(mul(1,2,3,4,5,6,7,8,9,10));
+
+let changeColor = () => {
+	let mylist = document.getElementsByClassName("listItems");
+	let i;
+	for (i = 0; i<mylist.length; i += 1){
+		mylist[i].style.color = "white";
+		mylist[i].style.backgroundColor = "black";
+	}
+}
+
